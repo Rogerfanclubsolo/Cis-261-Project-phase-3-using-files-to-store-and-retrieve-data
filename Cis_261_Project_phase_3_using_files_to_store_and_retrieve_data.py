c0cd8dc72152121bd71305a672bbf0427322cc8b
username <useremail>
2024-05-15 16:47:50 -0700

    git Progressing

--- Cis_261_Project_phase_3_using_files_to_store_and_retrieve_data.py
+++ Cis_261_Project_phase_3_using_files_to_store_and_retrieve_data.py
@@ -1,1 +1,68 @@
+def GetEmpName():
+    empname = input("Enter employee name: ")
+    return empname
 
+def GetDatesWorked():
+    fromdate = input("Enter Start Date (mm/dd/yyy): ")
+    todate = input("Enter End Date (mm/dd/yyyy): ")
+    return fromdate, todate
+
+def GetHoursWorked():
+    hours = float(input('Enter amount of hours worked: '))
+    return hours
+
+def GethourlyRate():
+    hourlyrate = float(input("enter hourly Rate: "))
+    return hourlyrate
+
+def GetTaxRate():
+    taxrate = float(input("enter tax rate: "))
+    return taxrate
+
+def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
+    grosspay = hours * hourlyrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
+
+def printinfo(EmpDetailList):
+    TotEmployees = 0 
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+    
+    for EmpList in EmpDetailList:
+        fromdate = EmpList[0]
+        todate = EmpList[1]
+        empname = EmpList[2]
+        hours = EmpList[3]
+        hourlyrate = EmpList[4]
+        taxrate = EmpList[5]
+        
+        grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
+        print(fromdate, todate, empname, f"{hours:,.2f}:, f"{hourlyrate:,.2f}", f"{grosspay:,.2f}", f"{taxrate:,.1%}", f"{incometax:,.2f}", f"{netpay:,.2f}")
+        
+        TotEmployees += 1
+        TotHours += hours
+        TotGrossPay += grosspay
+        TotTax += incometax
+        TotNetPay += netpay
+         
+        EmpTotals["TotEmp"] = TotEmployees
+        EmpTotals["TotHrs"] = TotHours
+        EmpTotals["TotGrossPay"] = TotGrossPay
+        EmpTotals["TotTax"] = TotTax
+        EmpTotals["TotNetPay"] = TotNetPay
+        
+def PrintTotals(EmpTotals):
+print()
+print(f"Total Number of Employees: {EmpTotals['TotEmp']}")
+Print(f"Total Hours Worked: {EmpTotals['TotHrs']}")
+print(f"Total Gross Pay: {EmpTotals['TotGrossPay']}")
+print(f"Total Income Tax: {empTotals[TotTax']:,.2f}")
+print(f"Total Net Pay: {EmpTotals['TotNetPay']:,.2f}")
+
+
+
+        
